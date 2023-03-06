37637f1498e33ebf25c8e132f09c39fd3f21ef20
alberto <albertokarri@gmail.com>
2023-03-06 20:16:00 -0300

    creando la base desde plantilla

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Font Awesome icons (free version) */}
+      <Script
+        src="https://use.fontawesome.com/releases/v6.1.0/js/all.js"
+        crossorigin="anonymous"
+      />
+      <Component {...pageProps} />
+      {/* Bootstrap core JS */}
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
+      {/* Core theme JS */}
+      <Script src="../js/scripts.js" />
+      <Script src="https://cdn.startbootstrap.com/sb-forms-latest.js" />
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        {/* Google fonts */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Varela+Round&display=optional"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <body id="page-top">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
