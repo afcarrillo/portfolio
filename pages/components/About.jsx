8c2dac93ff9a68e65be282e26ab78bbd5a359b44
alberto <albertokarri@gmail.com>
2023-03-10 13:08:00 -0300

    completado el about me

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -5,11 +5,23 @@ export default function About() {
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-lg-8">
             <h2 className="text-primary mb-4">About Me</h2>
+            <p className="text-white-50 mb-3">
+              My name is Alberto, I am graduated in Computer Science. I am from
+              Spain but I have lived in many countries for many years so I had
+              the opportunity to work in many international projects and in
+              multicultural teams and enviroments. These work and social
+              backgrounds makes me a collaborative person and good communicator.
+            </p>
+            <p className="text-white-50 mb-3">
+              I simply enjoy working on the code breaking down problems,
+              structuring a good architecture and writing clean and understable
+              code. I have dedication to continuously learning about new
+              technologies or methodologies to be ready for new solutions.
+            </p>
             <p className="text-white-50">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-              natus facilis consectetur expedita quisquam, pariatur doloribus
-              voluptatum accusantium reprehenderit aut, accusamus itaque, labore
-              laudantium? Ullam nihil quia odit praesentium eos?
+              While not working or studying I do practice rugby and every kind
+              of sports. In love with climbing mountains and travel by
+              motorbike.
             </p>
           </div>
         </div>
